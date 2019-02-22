b1d85b324e60b7ff4a4f86c7e66f595f0add3e38
carlos <shadows52@hotmail.com>
2019-02-22 17:26:17 +0000

    tercer commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 //CORS
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*, https://www.bastimex.com.mx");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", 'POST, GET, PUT, DELETE, OPTIONS')
     next();
@@ -70,5 +70,5 @@ app.use('/', appRoutes);
 //escuchar peticiones
 
 app.listen(8080, () => {
-    console.log('express server corriendo en el puerto ' + 8080 + '\x1b[32m%s\x1b[0m', 'online');
+    console.log('express server corriendo en el puerto ' + 8080 + ' \x1b[32m%s\x1b[0m', 'online');
 });
